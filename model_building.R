21d4345d071c7c08e72515b7ddfcbed2d5ce73fd
maskedstatistician <maskedstatistician@gmail.com>
2017-07-11 21:38:06 +0700

    lstm upload

--- model_building.R
+++ model_building.R
@@ -3,7 +3,7 @@ Sys.setlocale(locale = "thai")
 Sys.setlocale("LC_TIME", "thai")
 library(reticulate)
 #choose which python environment
-reticulate::use_python("C:/Anaconda/envs/py35")
+reticulate::use_python("Change the repertory of python ")
 #check if we are able to load keras
 model <- keras_model_sequential()
 library("data.table")
@@ -13,11 +13,10 @@ library("e1071")
 library("keras")
 
 #starts here:
-path <- 'D:/thaipatcut/V2'
-input_training <- paste0(path,'/input/article/')
-output_training <- paste0(path,'/output/training_set/')
-output_testing <- paste0(path,'/output/testing_set/')
-output_model <- paste0(path,'/output/model/')
+path <- 'Choose your repertory'
+output_training <- paste0(path,'/training_set/')
+output_testing <- paste0(path,'/testing_set/')
+output_model <- paste0(path,'/model/')
 
 x_train <- fread(paste(output_training,"x_train.csv",sep=""),colClasses=c(char="integer"))
 y_train <- fread(paste(output_training,"y_train.csv",sep=""))
@@ -34,36 +33,26 @@ y_train <- as.matrix(y_train)
 
 ##144 is the number of distinct character we can have
 
-model <- keras_model_sequential()
-model %>%
-  layer_embedding(144, 64, input_length = dim(x_train)[2]) %>% 
-  layer_dropout(0.25) %>%
-  layer_conv_1d(
-    64, 
-    3, 
-    padding = "valid",
-    activation = "relu",
-    strides = 1
-  ) %>%
-  layer_batch_normalization() %>%
-  layer_max_pooling_1d(2) %>%
-  layer_lstm(64) %>%
-  layer_batch_normalization() %>%
-  layer_dense(1) %>%
-  layer_activation("sigmoid")
-
-model %>% compile(
-  loss = "binary_crossentropy",
-  optimizer = "adam",
-  metrics = "accuracy"
+model <- keras_model_sequential() model %>%
+ layer_embedding(144, 64, input_length = dim(x_train)[2])  %>%
+ layer_lstm(64)  %>%
+ layer_activation("relu")  %>%
+ layer_dropout(0.25)  %>%
+ layer_dense(1)  %>%
+ layer_activation("sigmoid")
+ 
+model <- compile(
+ loss = "binary_crossentropy",
+ optimizer = "adam",
+ metrics = "accuracy"
+)
+ 
+history <- model %>% fit(
+ x_train, y_train,
+ epochs = 5, batch_size = 32
 )
 
-history <- model %>% 
-  fit(  x_train, 
-        y_train, 
-  epochs = 20, batch_size = 32)
-
 #Save the model 
-save_model_hdf5(model, filepath=paste0(output_model,'convlstm'), overwrite = TRUE,
+save_model_hdf5(model, filepath=paste0(output_model,'rnn'), overwrite = TRUE,
                 include_optimizer = TRUE)
 
